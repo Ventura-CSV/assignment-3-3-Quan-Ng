42efd288011fa3fef7653ecc9c8e4588927ea021
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-27 04:33:28 -0700

    ASSIG3.3:partial

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ def main():
     Code Your Program here
     ########################################
     """
+    # check if email starts with an alphabet character
+    if email[0].isalpha():
+        result = True
     
-    if (5 < email < 30):
+    elif 5 < len(email) < 30:         #check length of email and if letters are in the alphabet
+        result = True
+    elif: result = False:
+    elif '@' in email and '.' in email[email.index('@'):]:
         result = True
     else:
         result = False
+            
         
+    print(result)
         
 
     
